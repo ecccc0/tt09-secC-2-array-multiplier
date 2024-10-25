fe254353160d4a2556e755579628aec0992bf4a7
Eric Cheung <eric.ecccc@gmail.com>
2024-10-25 15:53:36 -0400

    test commit

--- src/project.v
+++ src/project.v
@@ -17,16 +17,44 @@ module tt_um_c_2_array_mult (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  wire [3:0] a, b, c;
-  assign a = ui_in[3:0];
-  assign b = uio_in[7:4];
-
+  .array_mult_structural(uio_in[3:0], uio_in[7:4], uio_out);
 
   assign uio_out = 0;
   assign uio_oe  = 0;
 
-  assign uo_out[7:4] = 4'b0;
-  assign  uo_out[3:0] = a + b;
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0, uio_in};
+endmodule
+
+module array_mult_structural (
+    input [3:0] m,
+    input [3:0] q,
+    output [7:0] p
+);
+    wire cout1, cout2, cout3, cout4, cout5, cout6, cout7, cout8, cout9, cout10, cout11, cout12;
+    wire s2, s3, s4, s6, s7, s8;
+    assign p[0] = m[0] & q[0];
+    full_adder fa1 (m[1] & q[0], m[0] & q[1], 1'b0, p[1], cout1);
+    full_adder fa2 (m[2] & q[0], m[1] & q[1], cout1, s2, cout2);
+    full_adder fa3 (m[3] & q[0], m[2] & q[1], cout2, s3, cout3);
+    full_adder fa4 (1'b0, m[3] & q[1], cout2, s4, cout4);
+    full_adder fa5 (s2, m[0] & q[2], 1'b0, p[2], cout5);
+    full_adder fa6 (s3, m[1] & q[2], cout5, s6, cout6);
+    full_adder fa7 (s4, m[2] & q[2], cout6, s7, cout7);
+    full_adder fa8 (m[3] & q[2], cout4, cout7, s8, cout8);
+    full_adder fa9 (m[0] & q[3], s6, 1'b0, p[3], cout9);
+    full_adder fa10 (m[1] & q[3], s7, cout9, p[4], cout10);
+    full_adder fa11 (m[2] & q[3], s8, cout10, p[5], cout11);
+    full_adder fa12 (m[3] & q[3], cout8, cout11, p[6], p[7]);
+
+
+endmodule
+
+module full_adder (
+    input a,b,cin,
+    output sum,carry
+);
+    assign sum = a ^ b ^ cin;
+    assign carry = (a & b) | (b & cin)  | (cin & a) ;
+
 endmodule
